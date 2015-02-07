189a619669565ff3236dbebda4aac71a88e47498
Kara A Carrell <karaAJC@gmail.com>
2015-02-07 14:52:41 -0600

    adding clean decks and working seed file, adding pry to gemfile

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,32 +1,37 @@
+require 'pry'
 #=======================================================================================================
 
 star_trek_deck = Deck.create(name: File.basename("db/decks/startrekdeck.txt", "deck.txt"))
 
 star_trek_cards = File.foreach('db/decks/startrekdeck.txt') do |line|
                     card_text = line.split(',')
-                    Card.create(word: card_text[0], definition: card_text[1], deck: star_trek_deck.id)
+                    Card.create(word: card_text[0], definition: card_text[1].chomp, deck: star_trek_deck)
                   end
 
 #=======================================================================================================
 
-git_commands_deck = Deck.create(name: File.basename("db/decks/gitcommandsdeck.txt", "deck.txt"))
+state_capitals_deck = Deck.create(name: File.basename("db/decks/statecapitalsdecknew.txt", "decknew.txt"))
 
-git_commands_deck = File.foreach('db/decks/gitcommands.txt') do |line|
-                      Card.create(word: line[0], definition: line[1], deck: git_commands_deck.id)
-                    end
-
-#=======================================================================================================
-
-state_capitals_deck = Deck.create(name: File.basename("db/decks/statecapitalsdeck.txt", "deck.txt"))
-
-state_capitals_deck = File.foreach('db/decks/statecapitalsdeck.txt') do |line|
-                        Card.create(word: line[0], definition: line[1], deck: state_capitals_deck.id)
+state_capitals_cards = File.foreach('db/decks/statecapitalsdecknew.txt') do |line|
+                        card_text = line.split(',')
+                        Card.create(word: card_text[0], definition: card_text[1], deck: state_capitals_deck)
                       end
 
 #=======================================================================================================
 
-oop_deck = Deck.create(name: File.basename("db/decks/OOPdeck.txt", "deck.txt").downcase)
+oop_deck = Deck.create(name: File.basename("db/decks/OOPdecknew.txt", "decknew.txt").downcase)
 
-oop_deck =  File.foreach('db/decks/OOPdeck.txt') do |line|
-              Card.create(word: line[0], definition: line[1], deck: oop_deck.id)
+oop_cards = File.foreach('db/decks/OOPdecknew.txt') do |line|
+              card_text = line.split(',')
+              Card.create(word: card_text[0], definition: card_text[1], deck: oop_deck)
             end
+
+#=======================================================================================================
+
+git_commands_deck = Deck.create(name: File.basename("db/decks/gitcommandsdecknew.txt", "decknew.txt"))
+
+git_commands_cards = File.foreach('db/decks/gitcommandsdecknew.txt') do |line|
+                        card_text = line.split(',')
+                        Card.create(word: card_text[0], definition: card_text[1], deck: git_commands_deck)
+                    end
+
