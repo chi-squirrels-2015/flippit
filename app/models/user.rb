30ea7a4d595735808186816e89586046380876b6
Ian Agne <ian.d.agne@gmail.com>
2015-02-07 19:45:51 -0600

    add pw authentication via user model / migration

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,16 +1,8 @@
 class User < ActiveRecord::Base
 
+  has_secure_password
 	has_many :rounds
-
 	validates :name, presence: true, uniqueness: true
 	validates :password, presence: true
-
-  def password=(plaintext)
-    self.password = BCrypt::Password.create(plaintext)
-  end
-
-  def authenticate(plaintext_password)
-    BCrypt::Password.new(self.password) == plaintext_password
-  end
 
 end
