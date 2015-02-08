3fd0714eda5378779d59cb660fb2dcd84f8def20
John Schulz <johnschulz75@gmail.com>
2015-02-07 19:34:03 -0600

    Creating controllers

---
+++ app/controllers/users.rb
@@ -0,0 +1,13 @@
+# users.rb
+post '/users/new' do
+	user = User.new
+	user.name = params[:name]
+	user.password= params[:password]
+	if user.save
+		redirect '/decks'
+	else
+		erb :'users/new'
+	end
+
+end
+
