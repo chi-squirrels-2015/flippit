b32ef741b79d1f1843f1e4489a0146343d88f95a
John Schulz <johnschulz75@gmail.com>
2015-02-09 08:15:01 -0600

    I can write anything I want here

--- app/controllers/decks.rb
+++ app/controllers/decks.rb
@@ -8,27 +8,31 @@ get '/decks/:deck_id' do
 	redirect '/login' unless session[:user_id]
 
 	@deck = Deck.find(params[:deck_id])
-	@round = Round.create(user_id: session[:user_id], deck_id: params[:deck_id] )
-
+	round = Round.find_or_create_by(user_id: session[:user_id], deck_id: params[:deck_id] )
+	session[:round_id] = round.id
 	@deck.cards.each do |card|
 		Guess.create(round: @round, card: card)
 	end
 
-	redirect "/decks/#{@deck.id}/round/#{@round.id}"
+	redirect "/decks/#{@deck.id}/round"
 end
 
 
-get '/decks/:deck_id/round/:round_id' do #this route sets up the card to be guessed
-	puts params
+get '/decks/:deck_id/round' do #this route sets up the card to be guessed
+	redirect '/login' unless session[:user_id]
 	@deck = Deck.find(params[:deck_id])
-	@round = Round.find(params[:round_id])
-	@cards = @deck.cards
-	@answers = @cards.pluck(:word).sample(4)
+	@round = Round.find(session[:round_id])
+	@card = @deck.cards.sample
+
+	answers = @deck.cards.pluck(:word).sample(3)
+	answers << @card.word
+	@choices = answers.shuffle
+
 	erb :'/rounds/cards/card'
 end
 
 post '/decks/:deck_id/round/:round_id' do #this route sets up the card to be guessed
-	puts params
+	redirect '/login' unless session[:user_id]
 	until no_cards # need to make this method
 		@available_cards = []
 
